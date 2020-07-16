a11bfeed88db545e1fd7f2b7bcaaa6226fadb7c6
Skil3e <manosm.design@gmail.com>
2020-07-16 11:36:56 +0300

    Initial commit

--- docz/src/gatsby-theme-docz/wrapper.js
+++ docz/src/gatsby-theme-docz/wrapper.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./main.scss";
 
-export default  ({ children }) => (
-    <div>
-        {children}
-    </div>
+export default ({children}) => (
+        <div>
+            {children}
+        </div>
 )

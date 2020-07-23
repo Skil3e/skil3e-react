ca175ffd255ab99cbbfaf2d08eeed3345344e952
Skil3e <manosm.design@gmail.com>
2020-07-23 13:40:40 +0300

    Initial commit

---
+++ docz/src/components/Loader/Loader.tsx
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import { FunctionComponent } from 'react';
+import { joinIgnoreEmpty } from "../utilities";
+import { Intents, Sizes } from "../types";
+
+const Loader: FunctionComponent<LoaderProps> =
+    ( {type , intent , dur, size, className, stroke , container, containerClassName} ) => {
+        const SvgAnimation: FunctionComponent<SvgAnimationProps> =
+            ( {type = "XML", atr, val, delay, repeat = "indefinite"} ) => <animate attributeType={ type } attributeName={ atr } values={ val } dur={ duration } repeatCount={ repeat } begin={ delay }/>
+        const ldrClass = joinIgnoreEmpty( " ", "loader", type, className, `fill--${intent}`, ( stroke ? "stroke--" + intent : "" ) );
+
+        const viewBox = "0 0 50 14";
+        const duration = dur + "ms";
+        const ldrContainer = joinIgnoreEmpty( " ", ( container ? "loader-container d-flex justify-content-center py-5" : "loader-container d-inline-flex" ), ( containerClassName ? containerClassName : "" ) );
+
+        let width = "";
+        if (size === 'xs') {
+            width = "20";
+        } else if (size === 'sm') {
+            width = "40";
+        } else if (size === 'md') {
+            width = "60";
+        } else if (size === 'lg') {
+            width = "80";
+        } else if (size === 'xl') {
+            width = "100";
+        }
+
+        return (
+            <div className={ ldrContainer }>
+                <svg className={ ldrClass } xmlns="http://www.w3.org/2000/svg" viewBox={ viewBox } width={ width }>
+                    { ( type === "cycle" ?
+                            <>
+                                <circle cx={ "8" } cy={ "7" } r={ "5" }>
+                                    <SvgAnimation atr={ "r" } val={ "0;5;" }/>
+                                </circle>
+                                <circle cx={ "8" } cy={ "7" } r={ "5" }>
+                                    <SvgAnimation atr={ "cx" } val={ "8;25;" }/>
+                                </circle>
+                                <circle cx={ "25" } cy={ "7" } r={ "5" }>
+                                    <SvgAnimation atr={ "cx" } val={ "25;42;" }/>
+                                </circle>
+                                <circle cx={ "42" } cy={ "7" } r={ "5" }>
+                                    <SvgAnimation atr={ "r" } val={ "5;0;" }/>
+                                </circle>
+
+                            </> : ""
+                    ) }
+
+                    { ( type === "pulse" ?
+                            <>
+                                <circle cx={ "8" } cy={ "7" } r={ "5" }>
+                                    <SvgAnimation atr={ "r" } val={ '5;2;2;5' }/>
+                                    <SvgAnimation atr={ "fill-opacity" } val={ "1;.5;.5;1" }/>
+                                </circle>
+                                <circle cx={ "25" } cy={ "7" } r={ "5" }>
+                                    <SvgAnimation atr={ "r" } val={ '5;2;2;5' } delay={ dur! / 4 + "ms" }/>
+                                    <SvgAnimation atr={ "fill-opacity" } val={ "1;.5;.5;1" } delay={ dur! / 4 + "ms" }/>
+                                </circle>
+                                <circle cx={ "42" } cy={ "7" } r={ "5" }>
+                                    <SvgAnimation atr={ "r" } val={ '5;2;2;5' } delay={ dur! / 2 + "ms" }/>
+                                    <SvgAnimation atr={ "fill-opacity" } val={ "1;.5;.5;1" } delay={ dur! / 2 + "ms" }/>
+                                </circle>
+                            </> : ""
+                    ) }
+                    { ( type === "flash" ?
+                            <>
+                                <rect x="3" y={ "2" } width={ "10" } height={ "10" }>
+                                    <SvgAnimation atr={ "fill-opacity" } val={ "1;.2;.2" }/>
+                                </rect>
+                                <rect x="20" y={ "2" } width={ "10" } height={ "10" }>
+                                    <SvgAnimation atr={ "fill-opacity" } val={ "1;.2;.2" } delay={ dur! / 4 + "ms" }/>
+                                </rect>
+                                <rect x="37" y={ "2" } width={ "10" } height={ "10" }>
+                                    <SvgAnimation atr={ "fill-opacity" } val={ "1;.2;.2" } delay={ dur! / 2 + "ms" }/>
+                                </rect>
+                            </> : ""
+                    ) }
+                </svg>
+            </div>
+        )
+    }
+
+Loader.defaultProps = {
+    type: "cycle",
+    intent: "accent",
+    dur: 500,
+    size: "sm",
+    stroke: false,
+    container: true
+}
+
+interface LoaderProps {
+    type?: "cycle" | "pulse" | "flash",
+    intent?: Intents,
+    size?: Sizes,
+    dur?: number,
+    className?: string,
+    containerClassName?: string,
+    stroke?: boolean,
+    container?: boolean
+}
+
+interface SvgAnimationProps {
+    type?: string,
+    atr: string,
+    val: string,
+    delay?: string,
+    repeat?: string
+}
+
+export default Loader;
---
+++ docz/src/components/Parallax/CSSParallax.tsx
@@ -0,0 +1,74 @@
+import React, { CSSProperties } from "react"
+
+interface ParallaxContainerProps {
+    backgroundColor: string
+    className: string
+    image: string
+    bgSize: "cover" | "contain"
+    bgPos: string,
+    style:  CSSProperties
+    offset: number,
+    reverseDirection: boolean,
+    speed: number,
+    repeat: "no-repeat" | "repeat" | "repeat-x" | "repeat-y"
+}
+
+export default class extends React.Component<ParallaxContainerProps> {
+    private node: HTMLDivElement | null | undefined;
+    constructor(props: ParallaxContainerProps) {
+        super(props);
+    }
+
+    componentDidMount() {
+        let windowWidth = document.documentElement.clientWidth;
+        const element = this.node!;
+        let speed = this.props.speed ? this.props.speed : 3;
+        let offset = this.props.offset ? this.props.offset : 0;
+
+        function parallax() {
+            const bound = element.getBoundingClientRect();
+            const trueAmount = (bound.bottom - window.innerWidth) - offset;
+            element.style.backgroundPositionY = Math.round(trueAmount / speed) + "px";
+        }
+
+        function parallaxReverse() {
+            const bound = element.getBoundingClientRect();
+            const trueAmount = (bound.bottom - window.innerWidth) + offset;
+            element.style.backgroundPositionY = -Math.round(trueAmount / speed) + "px";
+        }
+
+        if (windowWidth < 992) {
+            element.style.backgroundPositionY = 'center';
+        } else {
+            if (this.props.reverseDirection) {
+                parallaxReverse();
+                document.addEventListener('scroll', parallaxReverse);
+            } else {
+                parallax();
+                document.addEventListener('scroll', parallax);
+            }
+        }
+    }
+
+    render() {
+
+        const bgImageURL = this.props.image;
+        const size = (this.props.bgSize ? this.props.bgSize : 'cover');
+        const position = (this.props.bgPos ? this.props.bgPos : 'center');
+        const repeat = (this.props.repeat ? this.props.repeat : 'repeat-y');
+        const defaultStyle = {
+            backgroundColor: this.props.backgroundColor,
+            backgroundImage: 'url(' + bgImageURL + ')',
+            backgroundRepeat: repeat,
+            backgroundSize: size,
+            backgroundPositionX: position,
+        };
+        const Style = {...defaultStyle, ...this.props.style};
+
+        return (
+            <div ref={node => this.node = node} style={Style} className={this.props.className}>
+                {this.props.children}
+            </div>
+        )
+    }
+}

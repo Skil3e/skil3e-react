f7e3e8a22930a4514710204a11b285a8a383164c
Skil3e <manosm.design@gmail.com>
2020-08-01 15:01:12 +0300

    Initial Commit

---
+++ dist/Forms/PasswordInput.d.ts
@@ -0,0 +1,4 @@
+import * as React from "react";
+import { PasswordInputProps } from "../types";
+declare const PasswordInput: React.ForwardRefExoticComponent<PasswordInputProps & React.RefAttributes<HTMLInputElement>>;
+export default PasswordInput;

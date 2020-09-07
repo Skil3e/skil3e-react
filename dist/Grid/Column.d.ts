155625d5e444b1f5257f0d7c3eb3b1423a1ea950
Skil3e <manosm.design@gmail.com>
2020-09-07 13:57:26 +0300

    Initial commit

--- dist/Grid/Column.d.ts
+++ dist/Grid/Column.d.ts
@@ -3,8 +3,8 @@ import { BreakPoints, Pos } from "../types";
 import { HTMLAttributes } from "react";
 export interface ColumnProps extends HTMLAttributes<HTMLDivElement> {
     size?: number | BreakPoints<number>;
-    margin?: BreakPoints<Pos>;
-    padding?: BreakPoints<Pos>;
+    margin?: BreakPoints<Pos> | Pos;
+    padding?: BreakPoints<Pos> | Pos;
 }
 declare const Column: React.ForwardRefExoticComponent<ColumnProps & React.RefAttributes<HTMLDivElement>>;
 export default Column;
--- dist/Grid/FlexRow.d.ts
+++ dist/Grid/FlexRow.d.ts
@@ -2,8 +2,8 @@ import * as React from "react";
 import { BreakPoints, Pos } from "../types";
 import { HTMLAttributes } from "react";
 export interface FlexRowProps extends HTMLAttributes<HTMLDivElement> {
-    margin?: BreakPoints<Pos>;
-    padding?: BreakPoints<Pos>;
+    margin?: BreakPoints<Pos> | Pos;
+    padding?: BreakPoints<Pos> | Pos;
 }
 declare const FlexRow: React.ForwardRefExoticComponent<FlexRowProps & React.RefAttributes<HTMLDivElement>>;
 export default FlexRow;
